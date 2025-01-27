b506835b173574e4530241f04e5e96ea9476fbaa
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-01-27 16:16:41 +0300

    Initial Commit

---
+++ EggTimerTests/EggTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerTests.swift
+//  EggTimerTests
+//
+//  Created by Muhammed Mustafa Geldi on 27.01.2025.
+//
+
+import Testing
+@testable import EggTimer
+
+struct EggTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
